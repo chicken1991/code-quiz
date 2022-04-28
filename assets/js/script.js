5b366859fd43b5c7c2ba6813478eb614fd208890
benpitroff <benpitroff@gmail.com>
2022-04-27 19:36:25 -0700

    variables and all that jazz, rough start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // Score definitions THERE IS A BETTER WAY I'M SURE
 var score = 100; //Decrease for every wrong answer
-var question1 = document.querySelector("#1");
-var question2 = document.querySelector("#2");
-var question3 = document.querySelector("#3");
-var question4 = document.querySelector("#4");
+var question = document.querySelector("#question");
+var answer1 = document.getElementById("#btn1");
+var answer2 = document.getElementById("#btn2");
+var answer3 = document.getElementById("#btn3");
+var answer4 = document.getElementById("#btn4");
+
+
 
 // Where do I store the questions? Object and parse with JSON?
 
@@ -23,10 +26,11 @@ var question4 = document.querySelector("#4");
 var qObj1 = {
     theQuestion: "This is the first question. Choose wisely",
     rightAnswer: "Choose me!",
-    wrong1: "wrong answer 1",
-    wrong2: "wrong answer 2",
-    wrong3: "wrong answer 3"
+    wrongAnswer1: "wrong answer 1",
+    wrongAnswer2: "wrong answer 2",
+    wrongAnswer3: "wrong answer 3"
 }
+console.log(qObj1);
 
 var qObj2 = {
     theQuestion: "This is the second question. Choose wisely",
@@ -39,39 +43,55 @@ var qObj2 = {
 var qObj3 = {
     theQuestion: "This is the third question. Choose wisely",
     rightAnswer: "whatever",
-    wrong1: "wrong answer 1",
-    wrong2: "wrong answer 2",
-    wrong3: "wrong answer 3"
+    wrongAnswer1: "wrong answer 1",
+    wrongAnswer2: "wrong answer 2",
+    wrongAnswer3: "wrong answer 3"
 }
 
 var qObj4 = {
     theQuestion: "This is the fourth question. Choose wisely",
     rightAnswer: "whatever",
-    wrong1: "wrong answer 1",
-    wrong2: "wrong answer 2",
-    wrong3: "wrong answer 3"
+    wrongAnswer1: "wrong answer 1",
+    wrongAnswer2: "wrong answer 2",
+    wrongAnswer3: "wrong answer 3"
 }
 
+// array for the objects
 var objArray = [qObj1, qObj2, qObj3, qObj4];
+var btnClicker = document.querySelectorAll(".butt");
 
+console.log(btnClicker);
+//hardcoded stuff for fun
+// question.textContent = objArray[1].theQuestion;
+// answer1.textContent = objArray[1].rightAnswer;
 
-// ================= Display Section? =============================
+// array for the arrays < wtf?
 
 // Create a function that displays the Current Question, which will in turn display Answer Options
 function currentQuestion() {
-    for (var i = 0; i < objArray.length; i++){
-        question = objArray[i].theQuestion;
-        correct = objArray[i].rightAnswer;
-        wrong1 = objArray[i].wrongAnswer1;
-        wrong2 = objArray[i].wrongAnswer2;
-        wrong3 = objArray[i].wrongAnswer3;
-        console.log(question);
-    }
 
-    var answer1;
-    var answer2;
-    var answer3;
-    var answer4;
+    for (var i = 0; i < 1; i++){
+
+        // For question objArray[i] display the question and the answers
+        question.textContent = objArray[i].theQuestion;
+        answer1.textContent = objArray[i].rightAnswer;
+        answer2.textContent = objArray[i].wrongAnswer1;
+        answer3.textContent = objArray[i].wrongAnswer2;
+        answer4.textContent = objArray[i].wrongAnswer3;
+        // display each button thing for the li stuff and things
+        debugger;
+        //display objArray[i] until an answer is selected
+        btnClicker[i].addEventListener("click", function() {
+            if (objArray[i].rightAnswer){
+                console.log("Right");
+            } else {
+                console.log("WRONNNGGG");
+            }
+        });
+
+
+    }
 }
+
 
 currentQuestion();
